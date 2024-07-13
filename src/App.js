a8faad19081f20fa14065a6f744fd3de37eb4657
dicusa <jain.yash1909@gmail.com>
2024-07-14 01:46:02 +0530

    Nav Bar styling with active indicator

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import './App.css'; // Import the CSS file
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import "./App.css"; // Import the CSS file
 
 const App = () => {
   return (
-    <Router basename="/">
+    <Router basename="/my-portfolio">
       <div className="App">
         <nav className="navbar">
           <ul className="navbar-list">
-            <li><NavLink exact to="/" className="nav-link" activeClassName="active-link">Home</NavLink></li>
-            <li><NavLink to="/contact" className="nav-link" activeClassName="active-link">Contact</NavLink></li>
+            <li>
+              <NavLink exact to="/" className="nav-link">
+                {({ isActive, isPending, isTransitioning }) => (
+                  <span className={isActive ? "active-link" : ""}>Home</span>
+                )}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" className="nav-link">
+                {({ isActive, isPending, isTransitioning }) => (
+                  <span className={isActive ? "active-link" : ""}>Contact</span>
+                )}
+              </NavLink>
+            </li>
           </ul>
         </nav>
         <div className="content">
