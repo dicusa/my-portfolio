f88db8646d83559ddbcfaf0359b98e9721b336e3
dicusa <jain.yash1909@gmail.com>
2024-07-22 00:39:11 +0530

    major release

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,65 @@
-import React from 'react';
+import React from "react";
+import "../component/ContactPage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMailchimp,
+  faLinkedin,
+  faGithub,
+  faMedium,
+} from "@fortawesome/free-brands-svg-icons";
+import backgroundImage from "../asset/background.png";
+
+const contacts = [
+  {
+    title: "Email me at",
+    icon: faMailchimp,
+    link: "mailto:jain.yash1909@gmail.com",
+    description: "You can reach me via email for any inquiries.",
+    buttonText: "Send Email",
+  },
+  {
+    title: "LinkedIn Profile",
+    icon: faLinkedin,
+    link: "https://www.linkedin.com/in/yash-jain-869144b1/",
+    description: "Connect with me on LinkedIn.",
+    buttonText: "View LinkedIn",
+  },
+  {
+    title: "GitHub Profile",
+    icon: faGithub,
+    link: "https://github.com/dicusa",
+    description: "Check out my projects on GitHub.",
+    buttonText: "View GitHub",
+  },
+  {
+    title: "Medium Blog",
+    icon: faMedium,
+    link: "https://medium.com/@jain.yash1909",
+    description: "Read my latest articles on Medium.",
+    buttonText: "Read Blog",
+  },
+];
 
 const Contact = () => {
   return (
-    <div className="contact">
+    <div className="contact-container">
+      <div className="background-image"></div>
       <h1>Contact Me</h1>
-      <p>Email: your-email@example.com</p>
-      <p>LinkedIn: <a href="https://www.linkedin.com/in/your-profile">Your LinkedIn Profile</a></p>
-      <p>Phone: +1234567890</p>
+      <p>Reach me out via.</p>
+      <div className="contact-cards">
+        {contacts.map((contact, index) => (
+          <div key={index} className="contact-card">
+            <div className="icon-container">
+              <FontAwesomeIcon icon={contact.icon} size="2x" />
+            </div>
+            <h2>{contact.title}</h2>
+            <p>{contact.description}</p>
+            <a href={contact.link} className="cta-button">
+              {contact.buttonText}
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,48 +1,101 @@
-// src/components/Home.js
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { fetchMediumPosts, fetchGitHubRepos } from "../services/api";
 import "../component/Home.css";
+import { marked } from "marked";
+
 const Home = () => {
   const [mediumPosts, setMediumPosts] = useState([]);
-  const [githubRepos, setGitHubRepos] = useState([]);
   const [mediumPage, setMediumPage] = useState(1);
+  const [githubRepos, setGitHubRepos] = useState([]);
   const [repoPage, setRepoPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const mediumContainerRef = useRef(null);
+  const githubContainerRef = useRef(null);
 
   useEffect(() => {
     const loadMediumPosts = async () => {
+      setIsLoading(true);
       const posts = await fetchMediumPosts();
       setMediumPosts(posts.slice(0, 5));
+      setIsLoading(false);
     };
 
     const loadGitHubRepos = async () => {
+      setIsLoading(true);
       const repos = await fetchGitHubRepos();
-      setGitHubRepos(repos.slice(0, 5));
+      const reposWithReadme = await Promise.all(
+        repos.slice(repoPage * 5, (repoPage + 1) * 5).map(async (repo) => {
+          const readme = await fetchReadme(repo.owner.login, repo.name);
+          return { ...repo, readme };
+        })
+      );
+      setGitHubRepos(githubRepos.concat(reposWithReadme));
+      setRepoPage(repoPage + 1);
+      setIsLoading(false);
     };
 
     loadMediumPosts();
-    // loadGitHubRepos();
+    loadGitHubRepos();
   }, []);
 
   const loadMoreMediumPosts = async () => {
+    setIsLoading(true);
     const posts = await fetchMediumPosts();
     setMediumPosts(
       mediumPosts.concat(posts.slice(mediumPage * 5, (mediumPage + 1) * 5))
     );
     setMediumPage(mediumPage + 1);
+    setIsLoading(false);
   };
 
   const loadMoreGitHubRepos = async () => {
+    setIsLoading(true);
     const repos = await fetchGitHubRepos();
     setGitHubRepos(
       githubRepos.concat(repos.slice(repoPage * 5, (repoPage + 1) * 5))
     );
     setRepoPage(repoPage + 1);
+    setIsLoading(false);
   };
+
+  const handleScroll = (ref, loadMoreFunc) => {
+    if (
+      ref.current.scrollWidth - ref.current.scrollLeft <=
+        ref.current.clientWidth + 100 &&
+      !isLoading
+    ) {
+      loadMoreFunc();
+    }
+  };
+  const fetchReadme = async (owner, repo) => {
+    const response = await fetch(
+      `https://raw.githubusercontent.com/${owner}/${repo}/main/README.md`
+    );
+    if (response.ok) {
+      const text = await response.text();
+      return marked(text);
+    }
+    return null;
+  };
+  // const getReadmeImage = (repo) => {
+  //   const imageUrl = `https://raw.githubusercontent.com/${repo.owner.login}/${repo.name}/main/README.md`;
+  //   return (
+  //     <img
+  //       src={imageUrl}
+  //       alt={repo.name}
+  //       onError={(e) => (e.target.src = "https://via.placeholder.com/150")}
+  //     />
+  //   );
+  // };
 
   return (
     <div>
       <h2>Stories by Yash on Medium</h2>
-      <div className="scroll-container">
+      <div
+        className="scroll-container"
+        ref={mediumContainerRef}
+        onScroll={() => handleScroll(mediumContainerRef, loadMoreMediumPosts)}
+      >
         {mediumPosts.map((post, index) => (
           <div key={index} className="post-card">
             <a href={post.link} target="_blank" rel="noopener noreferrer">
@@ -52,23 +105,31 @@ const Home = () => {
           </div>
         ))}
       </div>
-      <button class="load-more-button" onClick={loadMoreMediumPosts}>
-        &#x21bb;
-      </button>
+      {isLoading && <p>Loading more posts...</p>}
 
       <h2>GitHub Repositories</h2>
-      <div className="scroll-container">
+      <div
+        className="scroll-container"
+        ref={githubContainerRef}
+        onScroll={() => handleScroll(githubContainerRef, loadMoreGitHubRepos)}
+      >
         {githubRepos.map((repo, index) => (
-          <div key={index} className="repo-card">
-            <h3>{repo.name}</h3>
-            <p>{repo.description}</p>
+          <div key={index} className="post-card">
             <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
-              View Repo
+              {repo.readme ? (
+                <div
+                  className="readme-content"
+                  dangerouslySetInnerHTML={{ __html: repo.readme }}
+                />
+              ) : (
+                <img src="https://via.placeholder.com/150" alt={repo.name} />
+              )}
+              <h3>{repo.name}</h3>
             </a>
           </div>
         ))}
       </div>
-      <button onClick={loadMoreGitHubRepos}>Load More</button>
+      {isLoading && <p>Loading more repositories...</p>}
     </div>
   );
 };
